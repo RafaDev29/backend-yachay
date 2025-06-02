adfe1136ac1239125a62741341f138742da9d57b
Rafael andres Rossel galarza <rafaelandresrosselgalarza@MacBook-Air-de-Rafael.local>
2025-06-02 06:42:59 -0500

    puerto 3030

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3030);
 }
 bootstrap();
