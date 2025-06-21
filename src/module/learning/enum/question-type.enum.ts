39ab6e4ffdc89ef867bb05fe75b8563bc508ae98
silverblade34 <mpacheco.tacay@gmail.com>
2025-06-21 15:21:45 -0500

    fix: question types enum

--- src/module/learning/enum/question-type.enum.ts
+++ src/module/learning/enum/question-type.enum.ts
@@ -5,8 +5,5 @@ export enum QuestionType {
   FILL_BLANK = 'fill_blank',
   DRAG_DROP = 'drag_drop',
   SEQUENCE_ORDER = 'sequence_order',
-  MATCH_PAIRS = 'match_pairs',
   SELECT_TEXT = 'select_text',
-  CATEGORIZE = 'categorize',
-  SHORT_ANSWER = 'short_answer'
 }
